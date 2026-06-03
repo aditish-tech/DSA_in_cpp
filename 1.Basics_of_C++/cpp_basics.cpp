2a3211e626e0b693a7f2df9de11b06f073a992eb
aditish-tech <aditisharma160427@gmail.com>
2026-06-03 23:37:02 +0530

    Add basic input/output and varibles

---
+++ 1.Basics_of_C++/cpp_basics.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Basic input and output
+    string name;
+    cout<<"Enter your name"<<endl;
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
+    
+    // Variables
+    int a=34;
+    char c='A';
+    float b=3.14;
+    cout<<"Integer :"<<a<<endl;
+    cout<<"Character :"<<b<<endl;
+    cout<<"Float :"<<c<<endl;
+}
