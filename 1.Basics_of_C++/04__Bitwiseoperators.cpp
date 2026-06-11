55178e098bbd7ea083314acf9b6256c9f8961cbd
aditish-tech <aditisharma160427@gmail.com>
2026-06-11 22:52:42 +0530

    Bitwise_Operators

---
+++ 1.Basics_of_C++/04__Bitwiseoperators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a =5,b=2;
+cout<<"The result for bitwise AND operator is: "<<(a&b)<<endl;
+cout<<"The result for bitwise OR operator is: "<<(a|b)<<endl;
+cout<<"The result for bitwise XOR operator is: "<<(a^b)<<endl;
+cout<<"The result for bitwise LEFT SHIFT operator is: "<<(a<<b)<<endl;
+cout<<"The result for bitwise RIGHT SHIFT operator is: "<<(a>>b)<<endl;
+}
